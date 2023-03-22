e6950a10cf3b0906e6ccf406c352c2d1b0c2c89e
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-22 18:13:26 +0100

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -9,10 +9,11 @@ const { createApp } = Vue
 
     created() {
         
-        for(let i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-                this.emailList.push(res.data.response);    
-            })  
-        }
+    for(let i = 0; i < 10; i++){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+      this.emailList.push(res.data.response);    
+      })  
+    }
+
     },   
   }).mount('#app')
